8b4bcfbce58b53ccb121da0e5fd51785ebe6ba6c
dd993 <dd993@drexel.edu>
2023-04-09 16:01:26 -0400

    used raw text in request body instead of json

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, request
 
-# create a Flask app
+# creates a Flask app
 app = Flask(__name__)
 
-# define a route for the /echo endpoint that accepts GET and POST requests
+# defines a route for the /echo endpoint that accepts GET AND POST requests
 @app.route('/echo', methods=['GET', 'POST'])
 def echo():
-    # check if the request method is POST
+    # checks if POST is the request method
     if request.method == 'POST':
         # extract the JSON data from the request body
-        data = request.json
+        data = request.data
 
         # return the data as the response
         return data
